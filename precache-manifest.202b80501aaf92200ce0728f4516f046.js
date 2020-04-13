8ade887c3115c298037fb937535d54720f41df21
Sergey Pomortsev <zaooooza@gmail.com>
2020-04-13 18:56:57 +0300

    Updates

---
+++ precache-manifest.202b80501aaf92200ce0728f4516f046.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4927da0dfe1e5a48953d8ff44caca562",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1787ec930de9da226569",
+    "url": "/static/css/main.e7503770.chunk.css"
+  },
+  {
+    "revision": "d1a185bfbf3c6ce1621b",
+    "url": "/static/js/2.8f10c816.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8f10c816.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1787ec930de9da226569",
+    "url": "/static/js/main.5974d1ff.chunk.js"
+  },
+  {
+    "revision": "d0e75cff622336571dd1",
+    "url": "/static/js/runtime-main.67c9d56e.js"
+  }
+]);
